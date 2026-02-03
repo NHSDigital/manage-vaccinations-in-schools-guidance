c3546849b5b4cf74eb082b135bfa7be1625e7954
Mike Thompson <mike@artifactual.co.uk>
2026-02-03 15:46:23 +0000

    Use a separate search index for national reporting guide
    This removes the default search and replaces it with two different
    searches with separate indexes.
    
    One indexes covers the national reporting user guide and the other
    covers everything else.

---
+++ app/lib/search.js
@@ -0,0 +1,30 @@
+/**
+ * Create a search index for a collection of items
+ * @param {string} permalink - The permalink of the search index
+ * @param {function} getItems - A function that returns a collection of items
+ * @returns {object} A search index object
+ */
+export const createSearchIndex = (permalink, getItems) => ({
+  data: () => ({ eleventyExcludeFromCollections: true, permalink }),
+  render({ collections }) {
+    const index = getItems(collections)
+      .map((item) => ({
+        //
+        // This logic taken directly from:
+        // https://github.com/x-govuk/govuk-eleventy-plugin/blob/4b13c204591a4afadf1af501612288e077e1ce5a/src/templates/search-index.11ty.js
+        //
+        title: item.data.title,
+        ...(item.data.description && { description: item.data.description }),
+        ...(item.data.eleventyNavigation.parent &&
+          item.data.eleventyNavigation.parent !== item.data.options.homeKey && {
+          section: item.data.eleventyNavigation.parent
+        }),
+        ...(item.data.date && { date: this.govukDate(item.data.date) }),
+        ...(item.url && { url: this.htmlBaseUrl(item.url, options.url) }),
+        ...(item.templateContent && {
+          tokens: this.tokenize(item.templateContent)
+        })
+      }))
+    return JSON.stringify(index, null, 2)
+  }
+})
